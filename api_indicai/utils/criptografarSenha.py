457466f34c3ae003b60dbb65d6490b9622bfe75d
eliasvictorr <eliasvictorr@hotmail.com>
2024-08-29 02:48:18 -0300

    Update criptografarSenha.py

--- api_indicai/utils/criptografarSenha.py
+++ api_indicai/utils/criptografarSenha.py
@@ -4,7 +4,6 @@ class CriptografarSenha:
 
     @staticmethod
     def hash_senha(senha: str) -> str:
-
         senha_bytes = senha.encode('utf-8')
         hash_bytes = bcrypt.hashpw(senha_bytes, bcrypt.gensalt())
         return hash_bytes.decode('utf-8')
@@ -12,5 +11,5 @@ class CriptografarSenha:
     @staticmethod
     def verificar_senha(senha: str, hash_senha: str) -> bool:
         senha_bytes = senha.encode('utf-8')
-        hash_bytes = hash.senha.encode('utf-8')
+        hash_bytes = hash_senha.encode('utf-8')
         return bcrypt.checkpw(senha_bytes, hash_bytes)
